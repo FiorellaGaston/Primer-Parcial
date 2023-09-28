ddac6ddb2335be6149dad08b8d1c9d244c87fbef
Fiorella <fiorellagaston05@gmail.com>
2023-09-28 20:55:23 -0300

    //condiciones básicas

---
+++ Primer Parcial Fiorella Gaston/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float horizontal;
+    public float vertical;
+
+    public float thrustForce;
+    public float rotationSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        // Direccion    * Magnitude   * Input    * DeltaTime.
+
+        transform.position += -transform.forward * thrustForce * vertical * Time.deltaTime;
+
+        transform.Rotate(Vector3.up, horizontal * Time.deltaTime * rotationSpeed);
+    }
+}
